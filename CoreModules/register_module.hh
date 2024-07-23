d003e78301da52ed9fb8590b1b72822ecded93c3
Dan Green <danngreen1@gmail.com>
2024-07-23 16:35:04 -0700

    Use std::function for register_module

--- CoreModules/register_module.hh
+++ CoreModules/register_module.hh
@@ -1,12 +1,13 @@
 #pragma once
 #include "CoreProcessor.hh"
 #include "elements/element_info_view.hh"
+#include <functional>
 #include <memory>
 
 namespace MetaModule
 {
 
-using CreateModuleFunc = std::unique_ptr<CoreProcessor> (*)();
+using CreateModuleFunc = std::function<std::unique_ptr<CoreProcessor>()>;
 
 bool register_module(std::string_view brand_name,
 					 std::string_view typeslug,
