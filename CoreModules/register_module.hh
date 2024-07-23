fc1b1f342d24e8eed8f5790e2ab9e3f529a60950
Dan Green <danngreen1@gmail.com>
2024-07-22 21:07:16 -0700

    Add interface for register_module()

---
+++ CoreModules/register_module.hh
@@ -0,0 +1,17 @@
+#pragma once
+#include "CoreProcessor.hh"
+#include "elements/element_info_view.hh"
+#include <memory>
+
+namespace MetaModule
+{
+
+using CreateModuleFunc = std::unique_ptr<CoreProcessor> (*)();
+
+bool register_module(std::string_view brand_name,
+					 std::string_view typeslug,
+					 CreateModuleFunc funcCreate,
+					 ModuleInfoView const &info,
+					 std::string_view faceplate_filename);
+
+} // namespace MetaModule
