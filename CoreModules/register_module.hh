bc14206dbbfd3083c8890c1a566e895d525f3079
Dan Green <danngreen1@gmail.com>
2024-10-15 07:07:15 -0700

    Add register_module template function to help create ModuleInfoView

--- CoreModules/register_module.hh
+++ CoreModules/register_module.hh
@@ -4,15 +4,29 @@
 #include <functional>
 #include <memory>
 
-namespace MetaModule
-{
+namespace MetaModule {
 
 using CreateModuleFunc = std::function<std::unique_ptr<CoreProcessor>()>;
 
-bool register_module(std::string_view brand_name,
-					 std::string_view typeslug,
-					 CreateModuleFunc funcCreate,
-					 ModuleInfoView const &info,
-					 std::string_view faceplate_filename);
+bool register_module(std::string_view brand_slug, std::string_view module_slug,
+                     CreateModuleFunc funcCreate, ModuleInfoView const &info,
+                     std::string_view faceplate_filename);
+
+template <typename ModuleT>
+bool register_module(std::string_view brand_name, std::string_view module_slug,
+                     ModuleInfoView const &info,
+                     std::string_view faceplate_filename) {
+  return register_module(
+      brand_name, module_slug, []() { return std::make_unique<ModuleT>(); },
+      info, faceplate_filename);
+}
+
+template <typename ModuleT, typename ModuleInfoT>
+bool register_module(std::string_view brand_name, std::string_view module_slug,
+                     std::string_view faceplate_filename) {
+  return register_module(
+      brand_name, module_slug, []() { return std::make_unique<ModuleT>(); },
+      ModuleInfoView::makeView<ModuleInfoT>(), faceplate_filename);
+}
 
 } // namespace MetaModule
