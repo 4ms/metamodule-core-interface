ae421c4716a61e5175213ba30decffb0d7a0d43e
Dan Green <danngreen1@gmail.com>
2025-01-27 15:03:23 -0800

    Add interface for async filebrowser

---
+++ filesystem/async_filebrowser.hh
@@ -0,0 +1,10 @@
+#pragma once
+#include <functional>
+
+// Interface matches Cardinal for ease of interoperability
+
+void async_dialog_filebrowser(const bool saving,
+							  const char *const defaultName,
+							  const char *const startDir,
+							  const char *const title,
+							  const std::function<void(char *path)> action);
