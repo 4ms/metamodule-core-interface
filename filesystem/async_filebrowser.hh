823ac75b85ec7998387df4a41ce8746788804d9e
Dan Green <danngreen1@gmail.com>
2025-01-27 16:13:50 -0800

    Change param name of async_filebrowser to indicate filtering by extension

--- filesystem/async_filebrowser.hh
+++ filesystem/async_filebrowser.hh
@@ -4,7 +4,7 @@
 // Interface matches Cardinal for ease of interoperability
 
 void async_dialog_filebrowser(const bool saving,
-							  const char *const defaultName,
+							  const char *const nameOrExtensions,
 							  const char *const startDir,
 							  const char *const title,
 							  const std::function<void(char *path)> action);
