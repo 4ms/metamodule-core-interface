ac527922ec2abaf6c8d8484c13b77a6c032f29a8
Dan Green <danngreen1@gmail.com>
2025-01-31 15:18:38 -0800

    Put stringify_osdialog_filters in async_filebrowser.hh. Delcare more interfaces for filebrowser

--- filesystem/async_filebrowser.hh
+++ filesystem/async_filebrowser.hh
@@ -1,5 +1,31 @@
 #pragma once
+#include "osdialog.h"
 #include <functional>
+#include <string>
+#include <string_view>
+
+void async_open_file(std::string_view initial_path,
+					 std::string_view filter_extension_list,
+					 std::string_view title,
+					 std::function<void(char *path)> &&action);
+
+void async_open_dir(std::string_view initial_path,
+					std::string_view filter_extension_list,
+					std::string_view title,
+					std::function<void(char *path)> &&action);
+
+void async_save_file(std::string_view initial_path,
+					 std::string_view filename,
+					 std::string_view title,
+					 std::function<void(char *path)> &&action);
+
+// Interface is close to rack's osdialog_file()
+
+void async_osdialog_file(osdialog_file_action action,
+						 const char *path,
+						 const char *filename,
+						 osdialog_filters *filters,
+						 std::function<void(char *path)> &&action_function);
 
 // saving: true if we are saving a file (not supported), false to open a file or dir.
 // nameOrExtensions:
@@ -18,4 +44,6 @@ void async_dialog_filebrowser(const bool saving,
 							  const char *const nameOrExtensions,
 							  const char *const startDir,
 							  const char *const title,
-							  const std::function<void(char *path)> action);
+							  const std::function<void(char *path)> action_function);
+
+std::string stringify_osdialog_filters(osdialog_filters *filters);
