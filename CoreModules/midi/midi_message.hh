6fbcda75dfd392a113c15b33a1a3dcac84714fc0
Dan Green <danngreen1@gmail.com>
2025-02-18 16:40:02 -0800

    Add Midi::toPrettyMultilineString()

--- CoreModules/midi/midi_message.hh
+++ CoreModules/midi/midi_message.hh
@@ -6,4 +6,5 @@ namespace MetaModule::Midi
 {
 
 std::string toPrettyString(std::span<uint8_t, 3> bytes);
-}
+std::string toPrettyMultilineString(std::span<uint8_t, 3> bytes);
+} // namespace MetaModule::Midi
