bcb82d9375914cb91a0529df0edce8ffb9b733c4
Dan Green <danngreen1@gmail.com>
2025-04-22 09:36:02 -0700

    Comments/doc for get/set_index

--- CoreModules/elements/elements_index.hh
+++ CoreModules/elements/elements_index.hh
@@ -6,50 +6,75 @@
 namespace MetaModule::ElementIndex
 {
 
-inline uint8_t get_index(const BaseElement &, ElementCount::Indices) {
-	return 0; //error
-}
-
-inline uint8_t get_index(const ParamElement &, ElementCount::Indices indices) {
+// get_index(element, indices):
+// Extracts the correct index from the indices, based on the element type.
+//
+// Example:
+// 		uint16_t idx = get_index(info.elements[i], info.indices[i]);
+//
+// If info.elements[i] derives from a ParamElement, for example, then
+// idx will equal info.indices[i].param_idx
+//
+inline uint16_t get_index(const ParamElement &, ElementCount::Indices indices) {
 	return indices.param_idx;
 }
 
-inline uint8_t get_index(const LightElement &, ElementCount::Indices indices) {
+inline uint16_t get_index(const LightElement &, ElementCount::Indices indices) {
 	return indices.light_idx;
 }
 
-inline uint8_t get_index(const JackInput &, ElementCount::Indices indices) {
+inline uint16_t get_index(const JackInput &, ElementCount::Indices indices) {
 	return indices.input_idx;
 }
 
-inline uint8_t get_index(const JackOutput &, ElementCount::Indices indices) {
+inline uint16_t get_index(const JackOutput &, ElementCount::Indices indices) {
 	return indices.output_idx;
 }
 
+inline uint16_t get_index(const BaseElement &, ElementCount::Indices) {
+	return ElementCount::Indices::NoElementMarker;
+}
+
+inline uint16_t get_index(const Element &element, ElementCount::Indices indices) {
+	return std::visit([=](auto const &el) { return get_index(el, indices); }, element);
+}
+
+// set_index(element, idx):
+// Given the element index and element type, this will set the correct field
+// in ElementCount::Indices and set all other fields to NoElementIndices marker.
+// Useful for constructing an array of indices from an array of elements.
+//
+// Usage example:
+//		auto indices = MetaModule::ElementIndex::set_index(info.element[i], idx);
+//
+// If `info.element[i]` derives from a ParamElement, then the returned `indices` will be:
+// {.param_idx = idx, .light_idx = NoElementIndices, .input_idx = NoElementIndices, .output_idx = NoElementIndices}
+//
+
 struct SetIndex {
-	uint8_t index;
+	uint16_t idx;
 
 	ElementCount::Indices operator()(const ParamElement &) {
 		ElementCount::Indices indices = ElementCount::NoElementIndices;
-		indices.param_idx = index;
+		indices.param_idx = idx;
 		return indices;
 	}
 
 	inline ElementCount::Indices operator()(const LightElement &) {
 		ElementCount::Indices indices = ElementCount::NoElementIndices;
-		indices.light_idx = index;
+		indices.light_idx = idx;
 		return indices;
 	}
 
 	inline ElementCount::Indices operator()(const JackInput &) {
 		ElementCount::Indices indices = ElementCount::NoElementIndices;
-		indices.input_idx = index;
+		indices.input_idx = idx;
 		return indices;
 	}
 
 	inline ElementCount::Indices operator()(const JackOutput &) {
 		ElementCount::Indices indices = ElementCount::NoElementIndices;
-		indices.output_idx = index;
+		indices.output_idx = idx;
 		return indices;
 	}
 
