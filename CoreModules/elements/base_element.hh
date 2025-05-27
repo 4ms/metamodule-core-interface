ec8ccf9f3507539254c1b58ae50831141cc2e6fe
Eric Gao <ericxgao@gmail.com>
2025-05-27 16:15:54 -0700

    fix: increase pos_names limit to 32

--- CoreModules/elements/base_element.hh
+++ CoreModules/elements/base_element.hh
@@ -77,7 +77,7 @@ struct Pot : ParamElement {
 	std::string_view units = "";
 	bool integral = false;
 	uint8_t display_precision = 0;
-	constexpr static size_t MaxPosNames = 16;
+	constexpr static size_t MaxPosNames = 32;
 	unsigned num_pos = 0;
 	std::array<std::string_view, MaxPosNames> pos_names{};
 };
