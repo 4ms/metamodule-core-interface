f7cbe2e3d366a7cd0cb5201bbe4ed3767a524a86
Dan Green <danngreen1@gmail.com>
2025-06-12 23:16:59 -0700

    On-demand resampler

--- dsp/resampler_one.hh
+++ dsp/resampler_one.hh
@@ -6,221 +6,139 @@
 namespace MetaModule
 {
 
-class ResamplerMono {
-public:
-	// get_input() returns float, and is only called when needed
-	// todo: before calling this, we need to know minimum samples required
-	float process(auto get_input) {
-		if (chan.ratio == 1.f) {
-			return get_input();
-		}
-
-		if (chan.flush) {
-			chan.flush = false;
-			chan.xm1 = 0;
-			chan.x0 = get_input();
-			chan.x1 = get_input();
-			chan.x2 = get_input();
-			chan.frac_pos = 0.0;
-		}
-
-		while (chan.frac_pos >= 1.f) {
-			// Optimize for resample rates >= 3
-			if (chan.frac_pos >= 3.f) {
-				chan.frac_pos = chan.frac_pos - 3.f;
-				chan.xm1 = chan.x1;
-				chan.x0 = get_input();
-				chan.x1 = get_input();
-				chan.x2 = get_input();
-			}
-
-			// Optimize for resample rates >= 2
-			if (chan.frac_pos >= 2.f) {
-				chan.frac_pos = chan.frac_pos - 2.f;
-				chan.xm1 = chan.x1;
-				chan.x0 = chan.x2;
-				chan.x1 = get_input();
-				chan.x2 = get_input();
-			}
-
-			// Optimize for resample rates >= 1
-			if (chan.frac_pos >= 1.f) {
-				chan.frac_pos = chan.frac_pos - 1.f;
-
-				// shift samples back one
-				// and read a new sample
-				chan.xm1 = chan.x0;
-				chan.x0 = chan.x1;
-				chan.x1 = chan.x2;
-				chan.x2 = get_input();
-			}
-		}
-
-		// calculate coefficients
-		float a = (3 * (chan.x0 - chan.x1) - chan.xm1 + chan.x2) / 2;
-		float b = 2 * chan.x1 + chan.xm1 - (5 * chan.x0 + chan.x2) / 2;
-		float c = (chan.x1 - chan.xm1) / 2;
-
-		auto out = (((a * chan.frac_pos) + b) * chan.frac_pos + c) * chan.frac_pos + chan.x0;
-		chan.frac_pos += chan.ratio;
-		return out;
-	}
-
-	void set_sample_rate_in_out(uint32_t input_rate, uint32_t output_rate) {
-		auto cur_ratio = (float)input_rate / (float)output_rate;
-
-		if (chan.ratio != cur_ratio) {
-			chan.ratio = cur_ratio;
-			chan.flush = true;
-		}
-	}
-
-	float ratio() {
-		return chan.ratio;
-	}
-
-	void flush() {
-		chan.flush = true;
-	}
-
-private:
-	struct Channel {
-		float ratio = 1;
-		bool flush{true};
-
-		float frac_pos{};
-		float xm1{};
-		float x0{};
-		float x1{};
-		float x2{};
-	};
-
-	Channel chan{};
-};
-
+template<size_t MAX_RESAMPLER_CHANNELS = 16>
 class Resampler {
 public:
-	Resampler(uint32_t num_channels = 2) {
-		num_chans = std::clamp<size_t>(num_channels, 1u, MAX_RESAMPLER_CHANNELS);
-
-		chans.resize(num_chans, Channel{});
-
-		input_stride = static_cast<uint8_t>(num_chans);
-		output_stride = static_cast<uint8_t>(num_chans);
+	Resampler(uint32_t num_channels = 1) {
+		set_num_channels(num_channels);
 	}
 
 	~Resampler() = default;
 
+	void set_num_channels(unsigned num_channels) {
+		num_chans = std::clamp<size_t>(num_channels, 1u, MAX_RESAMPLER_CHANNELS);
+		chans.resize(num_chans, Channel{});
+	}
+
+	// Gets one output per channel, and returns the value in output[]
 	// get_input() returns float, and is only called when needed
-	// todo: before calling this, we need to know minimum samples required
-	float process(uint32_t channel_index, auto get_input) {
-		if (channel_index >= num_chans)
-			return 0;
+	void process(auto get_input, std::span<float> output) {
 
-		Channel &chan = chans[channel_index];
+		if (output.size() < num_chans)
+			// error
+			return;
 
-		if (chan.ratio == 1.f) {
-			return get_input();
+		if (ratio == 1.f) {
+			for (auto &out : output) {
+				out = get_input();
+			}
+			return;
 		}
 
-		if (chan.flush) {
-			chan.flush = false;
-			chan.xm1 = 0;
-			chan.x0 = get_input();
-			chan.x1 = get_input();
-			chan.x2 = get_input();
-			chan.frac_pos = 0.0;
-		}
+		if (should_flush) {
+			should_flush = false;
+			frac_pos = 0.0;
 
-		while (chan.frac_pos >= 1.f) {
-			// Optimize for resample rates >= 3
-			if (chan.frac_pos >= 3.f) {
-				chan.frac_pos = chan.frac_pos - 3.f;
-				chan.xm1 = chan.x1;
+			for (auto &chan : chans) {
+				chan.xm1 = 0; // This causes some error when flushing, but less phase delay
 				chan.x0 = get_input();
+			}
+			for (auto &chan : chans)
 				chan.x1 = get_input();
+			for (auto &chan : chans)
 				chan.x2 = get_input();
+		}
+
+		while (frac_pos >= 1.f) {
+
+			if (frac_pos >= 3.f) {
+				frac_pos = frac_pos - 3.f;
+
+				for (auto &chan : chans) {
+					chan.xm1 = chan.x2;
+					chan.x0 = get_input();
+				}
+				for (auto &chan : chans)
+					chan.x1 = get_input();
+				for (auto &chan : chans)
+					chan.x2 = get_input();
 			}
 
-			// Optimize for resample rates >= 2
-			if (chan.frac_pos >= 2.f) {
-				chan.frac_pos = chan.frac_pos - 2.f;
-				chan.xm1 = chan.x1;
-				chan.x0 = chan.x2;
-				chan.x1 = get_input();
-				chan.x2 = get_input();
+			if (frac_pos >= 2.f) {
+				frac_pos = frac_pos - 2.f;
+
+				for (auto &chan : chans) {
+					chan.xm1 = chan.x1;
+					chan.x0 = chan.x2;
+					chan.x1 = get_input();
+				}
+				for (auto &chan : chans)
+					chan.x2 = get_input();
 			}
 
-			// Optimize for resample rates >= 1
-			if (chan.frac_pos >= 1.f) {
-				chan.frac_pos = chan.frac_pos - 1.f;
+			if (frac_pos >= 1.f) {
+				frac_pos = frac_pos - 1.f;
 
-				// shift samples back one
-				// and read a new sample
-				chan.xm1 = chan.x0;
-				chan.x0 = chan.x1;
-				chan.x1 = chan.x2;
-				chan.x2 = get_input();
+				for (auto &chan : chans) {
+					chan.xm1 = chan.x0;
+					chan.x0 = chan.x1;
+					chan.x1 = chan.x2;
+					chan.x2 = get_input();
+				}
 			}
 		}
 
 		// calculate coefficients
-		float a = (3 * (chan.x0 - chan.x1) - chan.xm1 + chan.x2) / 2;
-		float b = 2 * chan.x1 + chan.xm1 - (5 * chan.x0 + chan.x2) / 2;
-		float c = (chan.x1 - chan.xm1) / 2;
+		for (auto i = 0u; auto &chan : chans) {
+			float a = (3 * (chan.x0 - chan.x1) - chan.xm1 + chan.x2) / 2;
+			float b = 2 * chan.x1 + chan.xm1 - (5 * chan.x0 + chan.x2) / 2;
+			float c = (chan.x1 - chan.xm1) / 2;
 
-		auto out = (((a * chan.frac_pos) + b) * chan.frac_pos + c) * chan.frac_pos + chan.x0;
-		chan.frac_pos += chan.ratio;
-		return out;
+			output[i] = (((a * frac_pos) + b) * frac_pos + c) * frac_pos + chan.x0;
+			i++;
+		}
+		frac_pos += ratio;
+	}
+
+	float process(auto get_input) {
+		float output[1];
+		process(get_input, output);
+		return output[0];
 	}
 
 	void set_sample_rate_in_out(uint32_t input_rate, uint32_t output_rate) {
-		for (auto &chan : chans) {
-			auto cur_ratio = (float)input_rate / (float)output_rate;
-
-			if (chan.ratio != cur_ratio) {
-				chan.ratio = cur_ratio;
-				chan.flush = true;
-			}
+		auto cur_ratio = (float)input_rate / (float)output_rate;
+		if (ratio != cur_ratio) {
+			ratio = cur_ratio;
+			should_flush = true;
 		}
 	}
 
-	void set_input_stride(uint32_t stride) {
-		input_stride = stride;
-	}
-
-	void set_output_stride(uint32_t stride) {
-		output_stride = stride;
-	}
-
-	float ratio(unsigned chan) {
-		return chans[chan].ratio;
+	float resample_ratio(unsigned chan) {
+		return ratio;
 	}
 
 	void flush() {
-		for (auto &chan : chans) {
-			chan.flush = true;
-		}
+		should_flush = true;
 	}
 
 private:
-	struct Channel {
-		float ratio = 1;
-		bool flush{true};
+	float ratio = 1;
+	bool should_flush{true};
+	float frac_pos{};
 
-		float frac_pos{};
+	struct Channel {
 		float xm1{};
 		float x0{};
 		float x1{};
 		float x2{};
 	};
 
-	static constexpr size_t MAX_RESAMPLER_CHANNELS = 16;
 	FixedVector<Channel, MAX_RESAMPLER_CHANNELS> chans;
 
-	unsigned input_stride = 1;
-	unsigned output_stride = 1;
 	size_t num_chans = 0;
 };
+
+using ResamplerMono = Resampler<1>;
 
 } // namespace MetaModule
